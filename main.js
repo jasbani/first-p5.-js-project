5aef589ec8e5e9b1ad787559dbb5467bb8cfe187
jasbani <115411143+jasbani@users.noreply.github.com>
2023-05-26 20:02:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function preload()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 400);
+    canvas.position(70, 200);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw()
+{
+    image(video, 100, 80, 300, 250);
+  
+    stroke(168,10,10);
+    fill(168,10,10);
+    
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+
+    stroke(50,168,82);
+    fill(50,168,82);
+
+    rect(63,27,375,25);
+    rect(63,347,375,25);
+    rect(27,63,25,275);
+    rect(448,63,25,275);
+
+    image(video,100,80,300,250);
+}
+
+function take_snapshot()
+{
+ save('Mahi');
+}
+
+function filter_tint()
+{
+    tint_color = document.getElementById("color_name").value;
+}
